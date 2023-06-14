c5c0eda2c456b3f126b08a2fbda7d65991b15956
Jhesso <juho.hesso@hotmail.com>
2023-06-14 17:05:45 +0300

    playing with the readline function to get started

--- src/main.c
+++ src/main.c
@@ -6,16 +6,37 @@
 /*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 16:15:23 by jhesso            #+#    #+#             */
-/*   Updated: 2023/06/08 16:15:23 by jhesso           ###   ########.fr       */
+/*   Updated: 2023/06/14 17:00:55 by jhesso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*	minishell()
+*	The main loop of minishell
+*	Print a prompt, read user input and send that input to parsing
+*/
+void	minishell(void)
+{
+	char	*command_line;
+
+	while (1)
+	{
+		command_line = readline("minishell >");
+		if (!ft_strncmp(command_line, "exit", 4))
+			exit(EXIT_SUCCESS);
+		ft_putendl_fd(command_line, STDOUT_FILENO);
+		free (command_line);
+	}
+}
+
 int	main(int ac, char **av, char **envp)
 {
-	if (ac == 1)
-		return (0);
-	printf("%s", av[1]);
+	(void)ac;
+	(void)av;
+	(void)envp;
+	// init structs
+	minishell();
+	//exit shell
 	return (0);
 }
