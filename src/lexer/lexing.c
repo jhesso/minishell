1baf3d8584d5ee2a1ee7db3bd6818007e3cb1430
Dardan <dardangerguri@outlook.com>
2023-07-04 12:02:21 +0300

    the beginning of linked list, pushing so I can work on shcool's computer

--- src/lexer/lexing.c
+++ src/lexer/lexing.c
@@ -6,26 +6,43 @@
 /*   By: dardangerguri <dardangerguri@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 15:46:22 by dgerguri          #+#    #+#             */
-/*   Updated: 2023/06/29 17:13:15 by dardangergu      ###   ########.fr       */
+/*   Updated: 2023/07/04 12:01:27 by dardangergu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // static int	count_redirections(char **tokens, int)
-
-// t_commands	*command_type(char **tokens)
 // {
-// 	int array;
-// 	int i;
-
-// 	array = 0;
-// 	i = 0;
-// 	while (tokens[array])
-// 	{
-// 		if (ft_strrchr("<", tokens[array][i]))
-// 	}
+// 	while ()
 // }
+void	mem_allocation(char **tokens, int start)
+{
+	while (tokens[start])
+	{
+		count
+	}
+}
+
+t_commands	*command_type(char **tokens)
+{
+	t_commands	*commands; //change the name
+	int			array;
+	int			i;
+
+	commands = NULL;
+	array = 0;
+	i = 0;
+	while (tokens[array])
+	{
+		while (tokens[array][i] != '|')
+		{
+			mem_allocation(tokens, array);
+		}
+		array++;
+		commands = commands->next;
+	}
+}
 
 void	lexing(char *command_line, t_lexer *tokens)
 {
@@ -34,7 +51,7 @@ void	lexing(char *command_line, t_lexer *tokens)
 	quote_checker(command_line, tokens);
 	tokens->tokens = split_to_tokens(command_line, ' ');
 	syntax_checker(tokens->tokens);
-	// cmd = command_type(tokens->tokens);   //jhesso I have no idea what to name it :(
+	cmd = command_type(tokens->tokens);   //jhesso I have no idea what to name it :(
 
 
 	//This is to test the tokens!
