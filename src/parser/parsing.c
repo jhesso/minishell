e9f91f1fa4bb1bbb928ad662945fe621acee5244
jhesso <jhesso.dev@gmail.com>
2023-08-08 12:53:47 +0300

    added some more stuff in gitignore | added the loop in parse()

--- src/parser/parsing.c
+++ src/parser/parsing.c
@@ -6,7 +6,7 @@
 /*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 05:12:42 by jhesso            #+#    #+#             */
-/*   Updated: 2023/08/08 05:32:20 by jhesso           ###   ########.fr       */
+/*   Updated: 2023/08/08 12:50:17 by jhesso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,19 @@
 bool	parse(t_minihell *minihell)
 {
 	//TODO: move list creation here, remove quotes, expand variables, append correct path to command
-	if (remove_quotes(minihell->tokens) == false)
-		return (false);
-	if (expand_variables(minihell->tokens, minihell->env) == false)
-		return (false);
+	int	i;
+
+	i = 0;
+	while (minihell->tokens[i] != NULL)
+	{
+		if (remove_quotes(minihell->tokens[i]) == false)
+			return (false);
+		if (expand_variables(minihell->tokens[i], minihell->env) == false)
+			return (false);
+	}
 	if (create_lst_tokens(minihell->tokens) == false)
 		return (false);
-	if (append_path(minihell->lst_tokens, minihell->env) == false)
+	if (append_path(minihell->lst_tokens, minihell->env) == false) //TODO: I might be able to simply rip this out from my pipex
 		return (false);
 	return (true);
 }
