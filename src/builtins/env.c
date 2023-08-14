7feec16d94b326628a4f93dce47dae816c0aef9c
Dardan <dardangerguri@outlook.com>
2023-08-14 14:27:33 +0300

    Cleaned the function init_env from some unnecessary brackets

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 13:51:46 by dgerguri          #+#    #+#             */
-/*   Updated: 2023/08/11 19:19:07 by jhesso           ###   ########.fr       */
+/*   Updated: 2023/08/14 14:11:25 by dgerguri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,13 @@ void	init_env(t_minihell *minihell, char **envp)
 		i++;
 	minihell->env = malloc(sizeof(char *) * i + 1);
 	if (!minihell->env)
-	{
-		ft_putendl_fd("Error: Malloc error", STDERR_FILENO);
-		exit (EXIT_FAILURE);
-	}
+		malloc_error();
 	i = 0;
 	while (envp[i])
 	{
 		minihell->env[i] = ft_strdup(envp[i]);
 		if (!minihell->env[i])
-		{
-			ft_putendl_fd("Error: Malloc error", STDERR_FILENO);
-			exit (EXIT_FAILURE);
-		}
+			malloc_error();
 		i++;
 	}
 }
