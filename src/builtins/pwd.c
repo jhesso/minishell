9dccf9c29254427cd19a97706f2b738c3dc3d9b6
jhesso <jhesso.dev@gmail.com>
2023-08-23 05:31:41 +0300

    changed pwd_builtin() parameters from t_minihell *minihell -> void as we weren't using the parameter at all here

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 14:07:36 by dgerguri          #+#    #+#             */
-/*   Updated: 2023/08/21 14:11:31 by dgerguri         ###   ########.fr       */
+/*   Updated: 2023/08/23 05:05:53 by jhesso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	pwd_builtin(t_minihell *minihell)
+void	pwd_builtin(void)
 {
 	char	*pwd;
 
