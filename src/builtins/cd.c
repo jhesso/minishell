78169aaf5fe010bd9bcbd06388a80ec66747c1da
Dardan <dardangerguri@outlook.com>
2023-08-31 16:44:02 +0300

    Cleaned cd a little

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,41 +6,16 @@
 /*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 19:11:36 by dgerguri          #+#    #+#             */
-/*   Updated: 2023/08/30 03:53:42 by dgerguri         ###   ########.fr       */
+/*   Updated: 2023/08/31 16:28:32 by dgerguri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	cd_builtin(t_minihell *minihell)
+static void	modify_env(t_minihell *minihell, char *old_pwd, char *home, int flag)
 {
-	int		argv_size;
-	char	*old_pwd;
-	char	*home;
-	char	*pwd;
-	int		flag;
+	char *pwd;
 
-	flag = 0;
-	argv_size = count_strings(minihell->lst_tokens->argv);
-	old_pwd = ft_strjoin("OLDPWD=", get_value(ft_strdup("$PWD="), 5, minihell->env));
-	if (!old_pwd)
-		malloc_error();
-	home = get_value(ft_strdup("$HOME="), 6, minihell->env);
-	if (argv_size == 1)
-	{
-		if (!home[0])
-			printf("minishell: cd: HOME not set\n");
-		else
-			chdir(home);
-	}
-	else if (chdir(minihell->lst_tokens->argv[1]))
-	{
-		if (minihell->lst_tokens->argv[1][0] == '.')
-			printf("minishell: cd: %s: Not a directory\n", minihell->lst_tokens->argv[1]);
-		else
-			printf("minishell: cd: %s: No such file or directory\n", minihell->lst_tokens->argv[1]);
-		flag = 1;
-	}
 	pwd = ft_strjoin("PWD=", getcwd(NULL, 0));
 	if (!pwd)
 		malloc_error();
@@ -53,8 +28,41 @@ void	cd_builtin(t_minihell *minihell)
 		modify_variable(minihell, pwd);
 	}
 	else
+	{
+		printf("comes");
 		error_code = 1;
+	}
 	free(old_pwd);
 	free(home);
 	free(pwd);
+}
+
+void	cd_builtin(t_minihell *minihell)
+{
+	char	*old_pwd;
+	char	*home;
+	int		flag;
+
+	flag = 0;
+	old_pwd = ft_strjoin("OLDPWD=", get_value(ft_strdup("$PWD="), 5, minihell->env));
+	home = get_value(ft_strdup("$HOME="), 6, minihell->env);
+	if (!old_pwd || !home)
+		malloc_error();
+	if (count_strings(minihell->lst_tokens->argv) == 1)
+	{
+		if (chdir(home))
+		{
+			printf("minishell: cd: HOME not set\n");
+			flag = 1;
+		}
+	}
+	else if (chdir(minihell->lst_tokens->argv[1]))
+	{
+		if (minihell->lst_tokens->argv[1][0] == '.')
+			printf("minishell: cd: %s: Not a directory\n", minihell->lst_tokens->argv[1]);
+		else
+			printf("minishell: cd: %s: No such file or directory\n", minihell->lst_tokens->argv[1]);
+		flag = 1;
+	}
+	modify_env(minihell, old_pwd, home, flag);
 }
