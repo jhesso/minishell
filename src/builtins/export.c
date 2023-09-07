b47eca2a5878258d62b7ed5f5cc886146f3d4399
Dardan <dardangerguri@outlook.com>
2023-09-07 14:17:43 +0300

    Export error code fixed

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,32 +6,23 @@
 /*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 17:17:54 by dgerguri          #+#    #+#             */
-/*   Updated: 2023/09/06 20:02:50 by dgerguri         ###   ########.fr       */
+/*   Updated: 2023/09/07 14:16:19 by dgerguri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	check_validity(char *arg, int i)
+static int	check_validity(char *arg)
 {
-	int	flag;
+	int	i;
 
-	flag = 0;
-	if (!arg[i] || arg[i] == '=' || arg[i] == '+')
+	i = 0;
+	if (!arg[i] || arg[i] == '=' || arg[i] == '+' || ft_isdigit(arg[i]))
 		return (invalid_variable(arg, 1));
 	while (arg[i])
 	{
-		if (ft_isalpha(arg[i]))
+		if (ft_isalpha(arg[i]) || ft_isdigit(arg[i]) || arg[i] == '_')
 			i++;
-		else if (ft_isdigit(arg[i]) && !flag)
-			return (invalid_variable(arg, 1));
-		else if (ft_isdigit(arg[i]) && flag)
-			i++;
-		else if (arg[i] == '_')
-		{
-			flag = 1;
-			i++;
-		}
 		else if (arg[i] == '=')
 			break ;
 		else
@@ -42,6 +33,7 @@ static int	check_validity(char *arg, int i)
 
 void	export_builtin(t_minihell *mini, int i, int argv_size)
 {
+	g_global.error_code = 0;
 	argv_size = count_strings(mini->cmds->argv);
 	if (argv_size == 1)
 		while (mini->env[i])
@@ -51,7 +43,7 @@ void	export_builtin(t_minihell *mini, int i, int argv_size)
 		i = 1;
 		while (mini->cmds->argv[i])
 		{
-			if (!check_validity(mini->cmds->argv[i], 0))
+			if (!check_validity(mini->cmds->argv[i]))
 			{
 				if (ft_strrchr(mini->cmds->argv[i], '='))
 				{
@@ -60,7 +52,6 @@ void	export_builtin(t_minihell *mini, int i, int argv_size)
 					else
 						mini->env = export_variable(mini->env, \
 						mini->cmds->argv[i]);
-					g_global.error_code = 0;
 				}
 			}
 			i++;
